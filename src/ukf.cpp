3aa0ff2a241aed4b2b95defe5c5b4249c5b25816
dreamspring <33993878+dreamspring@users.noreply.github.com>
2018-01-19 22:53:20 -0500

    Add files via upload

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -19,10 +19,10 @@ UKF::UKF() {
   use_radar_ = true;
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 1.5;
+  std_a_ = 5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 0.5;
+  std_yawdd_ = 1;
   
   //DO NOT MODIFY measurement noise values below these are provided by the sensor manufacturer.
   // Laser measurement noise standard deviation position1 in m
